253b51ed32dd1b423439319e993c04d7a3264ae1
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-06-26 07:04:16 +0530

    first commit

---
+++ RockPapersScissors.py
@@ -0,0 +1,49 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+images = [rock, paper, scissors]
+
+user = int(input("Whats your choice?? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user >= 3 or user< 0: 
+    print("You typed an invalid number, you lose!") 
+else:
+    print(images[user])
+    computer= random.randint(0, 2)
+    print("Computer has chosen:")
+    print(images[computer])
+
+    if user == 0 and computer== 2:
+        print("You win!")
+    elif computer== 0 and user== 2:
+        print("You lose")
+    elif computer > user:
+        print("You lose")
+    elif user> computer:
+        print("You win!")
+    elif computer == user:
+        print("It's a draw")
